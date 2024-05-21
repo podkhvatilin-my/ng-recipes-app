f24116cf23095fcb600f106f84b8b2a5b8720109
Mykhailo Pidkhvatylin <michel.podkhvatilin@gmail.com>
2024-05-21 15:17:51 +0300

    observables and subjects into (end of module 6)

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
-import { Recipe } from './recipe.model';
 import { RecipeService } from './recipe.service';
+
 
 @Component({
   selector: 'app-recipes',
@@ -9,14 +9,4 @@ import { RecipeService } from './recipe.service';
   styleUrls: ['./recipes.component.css'],
   providers: [RecipeService]
 })
-export class RecipesComponent implements OnInit {
-  selectedRecipe: Recipe;
-
-  constructor(private recipeService: RecipeService) { }
-
-  ngOnInit() {
-    this.recipeService.recipeSelected.subscribe((recipe: Recipe) => {
-      this.selectedRecipe = recipe;
-    });
-  }
-}
+export class RecipesComponent { }
